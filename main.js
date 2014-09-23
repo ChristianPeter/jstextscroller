015fbda48b44f9212b5b9d735236822084ae21f6
Christian Peter <peter.chr@web.de>
2014-09-23 17:39:29 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// scroller plugin
+(function($, window, document) {
+		$.fn.textroller = function (duration) {			
+			var current = 0;
+			var items = $(this).children();			
+			
+			function switcher(){
+				if (current >= items.length) current = 0;
+				var item = items.eq(current);
+				item.fadeIn();
+				items.not(item).hide();
+				current ++;
+			}
+			
+			switcher();
+			setInterval(switcher, duration);
+		};
+	
+	
+})(jQuery, window, document);
+
+
+
+jQuery(document).ready(function(){
+	jQuery("#myroller").textroller(5000);
+});
